d35d81a152f5cf505cc2bac9905257c0d91962b3
zwh <24300@example.com>
2025-11-28 11:06:38 +0800

    zheng -first

--- cpu.py
+++ cpu.py
@@ -1,3 +1,31 @@
+import sys
+import json
+
+MEM_SIZE = 1 << 20
+
+STAT_AOK = 1
+STAT_HLT = 2
+STAT_ADR = 3
+STAT_INS = 4
+
+CC_NAMES = ["ZF", "SF", "OF"]
+
+REG_NAMES = ["rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi",
+             "r8", "r9", "r10", "r11", "r12", "r13", "r14"]
+
+I_HALT = 0
+I_NOP = 1
+I_RRMOVQ = 2
+I_IRMOVQ = 3
+I_RMMOVQ = 4
+I_MRMOVQ = 5
+I_OPQ = 6
+I_JXX = 7
+I_CALL = 8
+I_RET = 9
+I_PUSHQ = 10
+I_POPQ = 11
+
 class Simulator:
     def __init__(self):
         self.PC = 0
